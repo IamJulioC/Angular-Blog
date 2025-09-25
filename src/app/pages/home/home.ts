8c43e5bdcda98d18990224d675dcd27c1e1d9d0b
Júlio César Oliveira <iamjulioc@live.com>
2025-09-24 22:09:27 -0300

    Update component small-card

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BigCard } from "../../components/big-card/big-card";
 import { MenuTitle } from "../../components/menu-title/menu-title";
+import { SmallCard } from "../../components/small-card/small-card";
 
 @Component({
   selector: 'app-home',
-  imports: [BigCard, MenuTitle],
+  imports: [BigCard, MenuTitle, SmallCard],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
