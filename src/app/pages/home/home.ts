d1fc49d2074c1ea93528a6aca69188f80f2817e2
Júlio César Oliveira <iamjulioc@live.com>
2025-09-27 15:17:00 -0300

    Componented createds with styles

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { BigCard } from "../../components/big-card/big-card";
 import { MenuTitle } from "../../components/menu-title/menu-title";
 import { SmallCard } from "../../components/small-card/small-card";
+import { MenuBar } from '../../components/menu-bar/menu-bar';
 
 @Component({
   selector: 'app-home',
-  imports: [BigCard, MenuTitle, SmallCard],
+  imports: [BigCard, MenuTitle, SmallCard, MenuBar],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
